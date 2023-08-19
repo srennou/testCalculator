ae2e2b89f2dcaeb279c4c386cd63920c0d404a41
srennou <simosrennou@gmail.com>
2023-08-19 19:24:14 +0100

    prepare envirment

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 require('./bootstrap');
+import { createApp } from 'vue'
+
+import App from "./vue/app.vue";
+
+createApp(App).mount('#app')
---
+++ resources/js/vue/components/Calculator.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="app">
+        <h1>hello</h1>
+    </div>
+</template>
+  
+<script>
+
+export default {
+}
+</script>
+  
